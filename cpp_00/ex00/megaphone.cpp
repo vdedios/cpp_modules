aa7e8fdd17a1c8e2bb01047608fe85715a22c970
Victor <victor.dedios@outlook.com>
2021-05-06 21:17:00 +0200

    ex00 finished

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+
+static void upper(char **argv) {
+    int i = -1;
+
+    while ((*argv)[++i]) {
+        std::cout << (char)toupper((*argv)[i]);
+        if (!(*argv)[i + 1] && *(argv + 1)) {
+            std::cout << " ";
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    } else {
+        argv++;
+        while (*argv) {
+            upper(argv);
+            argv++;
+        }
+        std::cout << std::endl;
+    }
+}
