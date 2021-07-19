27deda0427efd2598e80849408f9bb3866c90019
Victor Dedios <victor.dedios@outlook.com>
2021-07-19 23:34:50 +0200

    finished cpp07

--- cpp_07/ex00/main.cpp
+++ cpp_07/ex00/main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <iomanip>
 #include "whatever.hpp"
+
+class Test {
+    public:
+        Test(): _num(0) {}
+        Test(int num): _num(num) {}
+        bool operator>(Test const & test) const { return this->_num > test.getNum(); }
+        bool operator<(Test const & test) const { return this->_num < test.getNum(); }
+        Test & operator=(Test const & test) {
+            this->_num = test.getNum();
+            return (*this);
+        }
+        int getNum(void) const { return this->_num; }
+
+    private:
+        int _num;
+};
+
+std::ostream & operator<<(std::ostream &o, Test const & t) {
+    o << t.getNum();
+    return o;
+}
 
 int main( void ) {
     int a = 2;
@@ -18,5 +40,10 @@ int main( void ) {
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
+    Test e(10);
+    Test f(12);
+    ::swap(e, f);
+    std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+    std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
     return 0;
 }
