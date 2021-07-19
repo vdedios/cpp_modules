f7b51f70d38ed670f4b2f46cb998587cf192fab5
Victor Dedios <victor.dedios@outlook.com>
2021-07-19 20:05:01 +0200

    add subject example

--- cpp_07/ex00/main.cpp
+++ cpp_07/ex00/main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include "whatever.hpp"
 
-int main(void) {
-    int a = 10;
+int main( void ) {
+    int a = 2;
     int b = 3;
 
-    swap<int>(a, b);
-    std::cout << "a: " << a << std::endl;
-    std::cout << "b: " << b << std::endl;
-    std::cout << max<int>(10, -1) << std::endl;
-    std::cout << min<int>(10, 10) << std::endl;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-    return (0);
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+    return 0;
 }
