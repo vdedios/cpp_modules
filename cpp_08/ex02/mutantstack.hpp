9f99c48f1f60c536aac92e4d3b4416f5f6b9091f
Victor Dedios <victor.dedios@outlook.com>
2021-07-20 22:43:33 +0200

    fix asignation operator

--- cpp_08/ex02/mutantstack.hpp
+++ cpp_08/ex02/mutantstack.hpp
@@ -3,14 +3,13 @@
 
 # include <stack>
 
-
 template<typename T>
 class MutantStack: public std::stack<T> {
     public:
         MutantStack(void) {}
         MutantStack(MutantStack const & mutantstack) { *this = mutantstack; }
         ~MutantStack(void) {}
-        MutantStack & operator=(MutantStack const & mutantstack) { *this = mutantstack; return *this; }
+        MutantStack & operator=(MutantStack const & mutantstack) { *this->c = mutantstack.c; return *this; }
 
         typedef typename std::stack<T>::container_type::iterator iterator;
         iterator begin(void) { return std::stack<T>::c.begin(); }
